7aaa058aefd03beae3fd79fdca2f48f24c6ef645
Ahmad Ali <aallii300300@gmail.com>
2020-07-26 05:50:07 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome, Supplier Or Restaurant</h1>
+        <div className="row mt-3 mb-3">
+          <button className="col-md-5 mr-2 btn btn-secondary text-white" > Register as Supplier </button>
+          <button className="col-md-5 ml-2 btn btn-secondary text-white" > Register as Restaurant </button>
+        </div>
+        <div className="row text-center mt-2 mb-2"><h1> OR </h1></div>
+        <div className="row text-center">
+          <button className="col-12 ml-1 mr-2 btn btn-secondary text-white mt-3" > LOG IN </button>
+        </div>
       </header>
     </div>
   );
