4881fa648dbdda595d4099294a48470e697126c0
Ahmad Ali <aallii300300@gmail.com>
2020-07-28 12:49:33 +0100

    update forms

--- src/RestaurantForm.js
+++ src/RestaurantForm.js
@@ -11,6 +11,9 @@ const RestaurantForm = ({ store, checkAuth }) => {
         role: "restaurant",
     })
 
+    const [submitted, setSubmitted] = useState(false)
+
+
 
 
     const changeRestaurantDetais = (e) => {
@@ -22,7 +25,7 @@ const RestaurantForm = ({ store, checkAuth }) => {
     }
 
     const submitNewRestaurant = (e) => {
-        //e.preventDefault();
+        e.preventDefault();
         //console.log(restaurant)
 
         fetch(`${domain}/restaurant/new`, {
@@ -33,6 +36,7 @@ const RestaurantForm = ({ store, checkAuth }) => {
             .then((res) => { return res.json(); })
             .then(result => {
                 console.log(result);
+                setSubmitted(true)
                 // console.log('allRestaurants', allRstaurants)
             }).catch((err) => console.log(err))
     }
@@ -40,39 +44,51 @@ const RestaurantForm = ({ store, checkAuth }) => {
 
     return (
         <div className="p-5">
-            <h1 className="mt-5 mb-5">Restaurant Registeration Form</h1>
-            <form onSubmit={submitNewRestaurant} >
-                <div className="form-group row">
-                    <label for="exampleInputEmail1" className="col-12 col-md-3">Email</label>
-                    <input type="email" className="form-control col-12 col-md-8" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your email"
-                        name="email" onChange={changeRestaurantDetais} />
-                </div>
 
-                <div className="form-group row">
-                    <label for="exampleInputPassword1" className="col-12 col-md-3">Password</label>
-                    <input type="password" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="Password"
-                        name="password" onChange={changeRestaurantDetais} />
-                </div>
-                <div className="form-group row">
-                    <label for="exampleInputPassword1" className="col-12 col-md-3">Name</label>
-                    <input type="text" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="eg. Flame London"
-                        name="name" onChange={changeRestaurantDetais}
-                    />
-                </div>
-                <div className="form-group row">
-                    <label for="exampleInputPassword1" className="col-12 col-md-3">Address</label>
-                    <input type="text" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="full addres with postcode"
-                        name="address" onChange={changeRestaurantDetais} />
-                </div>
-                <div className="form-group row">
-                    <label for="exampleInputPassword1" className="col-12 col-md-3">Phone</label>
-                    <input type="text" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="full number with country code"
-                        name="phone" onChange={changeRestaurantDetais} />
-                </div>
+            {submitted && <>
+                <h1>Thank you for Registerating</h1>
+                <a className="navbar-brand" href="/">
+                    <button className="btn btn-info"> <i class="material-icons">Go To Login </i></button>
+                </a>
+            </>}
 
-                <hr className="mt-3" />
-                <button type="submit" className="btn btn-info mb-5">Submit a New Restaurant</button>
-            </form>
+            {!submitted && <>
+
+                <h1 className="mt-5 mb-5">Restaurant Registeration Form</h1>
+                <form onSubmit={submitNewRestaurant} >
+                    <div className="form-group row">
+                        <label for="exampleInputEmail1" className="col-12 col-md-3">Email</label>
+                        <input type="email" className="form-control col-12 col-md-8" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your email"
+                            name="email" onChange={changeRestaurantDetais} />
+                    </div>
+
+                    <div className="form-group row">
+                        <label for="exampleInputPassword1" className="col-12 col-md-3">Password</label>
+                        <input type="password" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="Password"
+                            name="password" onChange={changeRestaurantDetais} />
+                    </div>
+                    <div className="form-group row">
+                        <label for="exampleInputPassword1" className="col-12 col-md-3">Name</label>
+                        <input type="text" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="eg. Flame London"
+                            name="name" onChange={changeRestaurantDetais}
+                        />
+                    </div>
+                    <div className="form-group row">
+                        <label for="exampleInputPassword1" className="col-12 col-md-3">Address</label>
+                        <input type="text" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="full addres with postcode"
+                            name="address" onChange={changeRestaurantDetais} />
+                    </div>
+                    <div className="form-group row">
+                        <label for="exampleInputPassword1" className="col-12 col-md-3">Phone</label>
+                        <input type="text" className="form-control col-12 col-md-8" id="exampleInputPassword1" placeholder="full number with country code"
+                            name="phone" onChange={changeRestaurantDetais} />
+                    </div>
+
+                    <hr className="mt-3" />
+                    <button type="submit" className="btn btn-info mb-5">Submit a New Restaurant</button>
+                </form>
+            </>
+            }
         </div>
     )
 }
