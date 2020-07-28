16b8f8e7bb4a1468629bf09a2ca0441728f8c42c
Ahmad Ali <aallii300300@gmail.com>
2020-07-28 02:34:45 +0100

    remote domain

--- src/config.js
+++ src/config.js
@@ -1,1 +1,19 @@
-export const domain = "http://localhost:5000"
+// let domain
+
+
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+
+
+export const domain = isLocalhost ? "http://localhost:5000" : "https://restaurant-supplier-api--ahmadali5.repl.co"
+
+
+// export const domain = "https://restaurant-supplier-api--ahmadali5.repl.co"
